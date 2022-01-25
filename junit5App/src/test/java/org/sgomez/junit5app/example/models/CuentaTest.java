e65cd9fc146d7db0ab72d747f157315498ae81f7
Fany Gómez <fanygomez1894@gmail.com>
2022-01-24 20:02:28 -0600

    Se modifico

--- junit5App/src/test/java/org/sgomez/junit5app/example/models/CuentaTest.java
+++ junit5App/src/test/java/org/sgomez/junit5app/example/models/CuentaTest.java
@@ -2,6 +2,7 @@ package org.sgomez.junit5app.example.models;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.sgomez.junit5app.example.exceptions.DineroInsuficienteException;
 
 import java.math.BigDecimal;
 
@@ -60,5 +61,60 @@ class CuentaTest {
         System.out.println(cuenta.getSaldo().toPlainString());
         //assertNotEquals(1100,cuenta.getSaldo().intValue());
         assertEquals("1100.12345",cuenta.getSaldo().toPlainString());
+    }
+    @Test
+    void testDineroInsuficienteExceptionCuenta() {
+        Cuenta cuenta = new Cuenta("Sebas", new BigDecimal("1000.12345"));
+
+       Exception exception = assertThrows(DineroInsuficienteException.class,()->{
+            cuenta.debito(new BigDecimal(1500));
+        });
+       String msjActual = exception.getMessage();
+       String msjEsperado = "Dinero Insuficiente";
+       assertEquals(msjEsperado,msjActual);
+       System.out.println("Dinero Insufiente: Passed");
+    }
+
+    @Test
+    void testTransFerirDineroCuentas() {
+        Cuenta cuenta1 = new Cuenta("Sebas", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Jose", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco("Banco Agri");
+        banco.transferir(cuenta2,cuenta1, new BigDecimal(500));
+
+        assertEquals("1000.8989",cuenta2.getSaldo().toPlainString());
+        assertEquals("3000",cuenta1.getSaldo().toPlainString());
+
+    }
+
+    @Test
+    void testRelacionBancoCuentas() {
+        Cuenta cuenta1 = new Cuenta("Jose", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Sebas", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco("Banco Agri");
+        banco.addCuenta(cuenta1);
+        banco.addCuenta(cuenta2);
+
+        banco.transferir(cuenta2,cuenta1, new BigDecimal(500));
+
+        assertEquals("1000.8989",cuenta2.getSaldo().toPlainString());
+        assertEquals("3000",cuenta1.getSaldo().toPlainString());
+        //validar relacion
+        //cuenta -> bancoss
+        assertEquals(2,banco.getCuentas().size());
+        //Relacion banco -> cuenta
+        assertEquals("Banco Agri",cuenta1.getBanco().getNombre());
+
+        //Buscar en la lista de cuentas el nombre de Sebas
+        assertEquals("Sebas", banco.getCuentas().stream().
+                filter(cuenta -> cuenta.getPersona().equals("Sebas"))
+                    .findFirst()
+        .get().getPersona());
+
+        //
+        assertFalse(banco.getCuentas().stream().
+                anyMatch(cuenta -> cuenta.getPersona().equals("Jose G")));
     }
 }
